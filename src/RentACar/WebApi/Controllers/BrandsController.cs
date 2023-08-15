6dc0ae22764288557863ac9fd58370da1f7116dd
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-08-15 17:21:21 +0300

    Init Create

---
+++ src/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Common;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse result = await Mediator.Send(createBrandCommand);
+
+           return Ok(result);
+        }
+    }
+}
